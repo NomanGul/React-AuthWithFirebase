20f1384c907fc4e0948b9b43f431229e733584f0
Noman Gul <nomangul2001@gmail.com>
2018-09-13 11:43:46 +0500

    BS form code added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="container">
+        <form className="mt-5 col-md-5">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your password with anyone else.
+            </small>
+          </div>
+          <button type="submit" class="btn btn-primary mr-5">
+            LogIn
+          </button>
+          <button type="submit" class="btn btn-primary">
+            SignUp
+          </button>
+        </form>
       </div>
     );
   }
